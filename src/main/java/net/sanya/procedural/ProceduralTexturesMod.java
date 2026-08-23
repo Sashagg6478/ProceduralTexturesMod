d7ddd46b0497c37fdbbcb173e498b15783dd4546
Sashagg6478 <suknevsasa7@gmail.com>
2026-08-23 13:27:32 +1000

    Update ProceduralTexturesMod.java

--- src/main/java/net/sanya/procedural/ProceduralTexturesMod.java
+++ src/main/java/net/sanya/procedural/ProceduralTexturesMod.java
@@ -2,7 +2,7 @@ package net.sanya.procedural;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("proceduraltexturesmod")
 public class ProceduralTexturesMod {
